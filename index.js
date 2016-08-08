7dbb825c2d7e4380441130f32667665cb42a9fe5
Amio <amio.cn@gmail.com>
2016-08-08 23:38:01 +0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+const tree = require('directory-tree')
+const parseArgs = require('minimist')
+
+const argv = parseArgs(process.argv.slice(2))
+const dir = argv._[0] || process.cwd()
+
+const treeJSON = tree(dir)
+const transformedTreeJSON = JSON.parse(JSON.stringify(treeJSON), function (k, v) {
+  switch (k) {
+    case 'path':
+      return v.replace(dir, '')
+    case 'size':
+      this.value = v
+      return
+    default:
+      return v
+  }
+})
+// console.log(transformedTreeJSON)
+fs.writeFileSync('treedata-map.json', JSON.stringify(treeJSON, null, 2))
+fs.writeFileSync('treedata-flame.json', JSON.stringify(transformedTreeJSON, null, 2))
