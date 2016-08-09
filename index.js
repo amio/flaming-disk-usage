74922b4bd3fb44d8f88cff052a03cc3015a5cf8d
Amio <amio.cn@gmail.com>
2016-08-09 14:30:57 +0800

    Sum children's size to parents.

--- index.js
+++ index.js
@@ -13,26 +13,28 @@ const transformedTreeJSON = JSON.parse(JSON.stringify(treeJSON), function (k, v)
     case 'path':
       return v.replace(dir, '')
     case 'size':
-      this.value = v
-      return
+      this.value = this.value || this.size
+      return v
+    case 'children':
+      this.value = this.children.reduce(function (prev, curr) {
+        return prev + curr.size
+      }, this.size)
+      return v
     default:
       return v
   }
 })
 
-// treeJSON.name = transformedTreeJSON.name = path.resolve(dir)
-
-// console.log(treeJSON)
-// console.log(transformedTreeJSON)
-const treeFlameData = `
+// Write to data js
+const treeFlameDataJS = `
   window.treedir = '${path.resolve(dir)}';
   window.treedata = ${JSON.stringify(transformedTreeJSON)};
 `
 fs.writeFileSync(
   path.join(__dirname, 'tree-flame-data.js'),
-  treeFlameData
+  treeFlameDataJS
 )
-// fs.writeFileSync('treedata-map.json', JSON.stringify(treeJSON, null, 2))
 
+// Open flame graph html
 opn(path.join(__dirname, 'tree-flame.html'))
 process.exit()
