ff3fbdd625c82548f2107d4170008e7958a26bdf
Amio <amio.cn@gmail.com>
2016-08-13 12:18:35 +0800

    Add cli help info.

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 const fs = require('fs')
 const path = require('path')
-const tree = require('directory-tree')
+const tree = require('@amio/directory-tree')
 const opn = require('opn')
 
-const argv = require('minimist')(process.argv.slice(2))
-const dir = argv._[0] || process.cwd()
+module.exports = function inspectDiskUsage (dir) {
 
-const treeJSONString = JSON.stringify(tree(dir))
-const transformedTreeJSON = JSON.parse(treeJSONString, function (k, v) {
-  switch (k) {
-    case 'path':
-      return v.replace(dir, '')
-    case 'size':
-      this.value = this.value || this.size
-      return v
-    case 'children':
-      this.value = countSize(this)
-      return v
-    default:
-      return v
-  }
-})
+  const treeJSONString = JSON.stringify(tree(dir))
+  const transformedTreeJSON = JSON.parse(treeJSONString, function (k, v) {
+    switch (k) {
+      case 'path':
+        return v.replace(dir, '')
+      case 'size':
+        this.value = this.value || this.size
+        return v
+      case 'children':
+        this.value = countSize(this)
+        return v
+      default:
+        return v
+    }
+  })
 
-// Recursively count target's children's size.
-function countSize (target) {
-  if (target.children) {
-    return target.children.reduce((prev, curr) => {
-      return prev + countSize(curr)
-    }, 0)
-  } else {
-    return target.size
+  // Recursively count target's children's size.
+  function countSize (target) {
+    if (target.children) {
+      return target.children.reduce((prev, curr) => {
+        return prev + countSize(curr)
+      }, 0)
+    } else {
+      return target.size
+    }
   }
+
+  // Write to data js
+  const treeFlameDataJS = `
+    window.treedir = '${path.resolve(dir)}';
+    window.treedata = ${JSON.stringify(transformedTreeJSON)};
+  `
+  fs.writeFileSync(
+    path.join(__dirname, 'tree-flame-data.js'),
+    treeFlameDataJS
+  )
+
+  // Open flame graph html
+  opn(path.join(__dirname, 'tree-flame.html'), { wait: false })
 }
-
-// Write to data js
-const treeFlameDataJS = `
-  window.treedir = '${path.resolve(dir)}';
-  window.treedata = ${JSON.stringify(transformedTreeJSON)};
-`
-fs.writeFileSync(
-  path.join(__dirname, 'tree-flame-data.js'),
-  treeFlameDataJS
-)
-
-// Open flame graph html
-opn(path.join(__dirname, 'tree-flame.html'), { wait: false })
