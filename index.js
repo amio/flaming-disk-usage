4e82cbe8c98daced74cc306a3c452f4865d452cd
Amio <amio.cn@gmail.com>
2016-08-16 16:52:02 +0800

    Use custom tree-walker.js. Close #1.

--- index.js
+++ index.js
@@ -1,42 +1,16 @@
 const fs = require('fs')
 const path = require('path')
-const tree = require('directory-tree')
+const generateTreeJSON = require('./lib/generate-tree-json.js')
 const opn = require('opn')
 
 const argv = require('minimist')(process.argv.slice(2))
 const dir = argv._[0] || process.cwd()
-
-const treeJSONString = JSON.stringify(tree(dir))
-const transformedTreeJSON = JSON.parse(treeJSONString, function (k, v) {
-  switch (k) {
-    case 'path':
-      return v.replace(dir, '')
-    case 'size':
-      this.value = this.value || this.size
-      return v
-    case 'children':
-      this.value = countSize(this)
-      return v
-    default:
-      return v
-  }
-})
-
-// Recursively count target's children's size.
-function countSize (target) {
-  if (target.children) {
-    return target.children.reduce((prev, curr) => {
-      return prev + countSize(curr)
-    }, 0)
-  } else {
-    return target.size
-  }
-}
+const treeJSON = generateTreeJSON(dir)
 
 // Write to data js
 const treeFlameDataJS = `
   window.treedir = '${path.resolve(dir)}';
-  window.treedata = ${JSON.stringify(transformedTreeJSON)};
+  window.treedata = ${JSON.stringify(treeJSON)};
 `
 fs.writeFileSync(
   path.join(__dirname, 'tree-flame-data.js'),
